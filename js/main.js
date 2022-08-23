1e8e25934b3d3f08aa85dd64a24ed62ecb88c7ce
Ernst Thelifort <ethelifort@gmail.com>
2022-08-22 22:57:33 -0400

    Fetch requests added, Logic and DOM manipulation remaining

--- js/main.js
+++ js/main.js
@@ -1,26 +1,81 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.getElementById('naruto').addEventListener('click', getFetch)
+document.getElementById('bleach').addEventListener('click', getFetch2)
+document.getElementById('onepiece').addEventListener('click', getFetch3)
+document.getElementById('dragonball').addEventListener('click', getFetch4)
 
+//Fetching data for naruto
 function getFetch(){
-  const choice = document.querySelector('input').value
+  const choice = document.getElementById('naruto').value
   console.log(choice);
-
-  const url = `https://openlibrary.org/isbn/${choice}.json`
+  const url = `https://animechan.vercel.app/api/quotes/anime?title=${choice}`; 
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
 
         console.log(data);
-
-        
-      
-       
-
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+
+
+//Fetching data for bleach
+function getFetch2(){
+  const choice = document.getElementById('bleach').value
+  console.log(choice);
+  const url = `https://animechan.vercel.app/api/quotes/anime?title=${choice}`; 
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+
+        console.log(data);
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+//Fetching data for one piece
+
+function getFetch3(){
+  const choice = document.getElementById('onepiece').value
+  console.log(choice);
+  const url = `https://animechan.vercel.app/api/quotes/anime?title=${choice}`; 
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+
+        console.log(data);
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+// Fetching data for Dragon ball z
+
+function getFetch4(){
+  const choice = document.getElementById('dragonball').value
+  console.log(choice);
+  const url = `https://animechan.vercel.app/api/quotes/anime?title=${choice}`; 
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+
+        console.log(data);
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+
+
+
+ 
 
  
